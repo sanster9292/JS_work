b26d65946d2a369e62ac2748e7544de9b73df7f3
Sanwal Yousaf <sanster9292@users.noreply.github.com>
2018-09-09 14:32:44 -0400

    uploaded the code file
    Day 0 of 10 of JS

---
+++ 10daysOfJS/day0of10.js
@@ -0,0 +1,26 @@
+
+/**
+This intro exercise is one of the most convoluted pieces of intro exercises
+I have ever seen. I have some experience with JavaScript experience and This
+is by far the most convoluted intro Hello World exercise I haev ever come across.
+
+This would daunt and might completely daunt a complete  newcomer and might
+turn some people off completely.
+
+**/
+
+
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+   console.log(parameterVariable)
+}
